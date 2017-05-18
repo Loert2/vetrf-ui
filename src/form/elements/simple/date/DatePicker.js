f1faa9dc055fa3d9c3e37e13f8674aa1d7f76aec
morozov-ev-131216 <morozov-ev-131216@fsvps.ru>
2017-05-18 12:17:22 +0300

    Валидация DatePicker в onChange

--- src/form/elements/simple/date/DatePicker.js
+++ src/form/elements/simple/date/DatePicker.js
@@ -1,38 +1,53 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { PureComponent } from 'react';
 import Datetime from 'react-datetime';
 import Moment from 'moment';
+import debounce from 'lodash/debounce';
 /**
 * Компонент-обертка для Datetime из react-datetime: https://github.com/YouCanBookMe/react-datetime
 * */
-const DatePicker = ({ value, onChange, className, inputProps, id, validate }) => (
-   <Datetime dateFormat="DD.MM.YYYY"
-             id={ id }
-             locale="ru"
-             value={ value || "" }
-             onChange={
-                (m) => onChange && onChange(m && m.format ? m.format("DD.MM.YYYY") : m)
-             }
-             className={ className }
-             timeFormat={ false }
-             onBlur={
-                () => {
-                   if (validate) {
-                      validate(value);
-                   } else {
-                      Moment.locale("ru");
-                      const formats = ["DD-MM-YYYY", "DD/MM/YYYY", "DD.MM.YYYY"];
-                      if (Moment(value, formats).isValid()) {
-                         onChange && onChange(Moment(value, formats).format("DD.MM.YYYY"));
-                      } else {
-                         onChange && onChange(Moment().format("DD.MM.YYYY"));
+class DatePicker extends PureComponent {
+   constructor(props, context) {
+      super(props, context);
+      this.validateFormat = props.validate ? debounce(props.validate, 400) : null;
+   }
+
+   render() {
+      const { value, onChange, className, inputProps, id, validate } = this.props;
+      return(
+         <Datetime dateFormat="DD.MM.YYYY"
+                   id={ id }
+                   locale="ru"
+                   value={ value || "" }
+                   onChange={
+                      (m) => {
+                         const val = m && m.format ? m.format("DD.MM.YYYY") : m;
+                         onChange && onChange(val);
+                         this.validateFormat(val);
                       }
                    }
-                }
-             }
-             disableOnClickOutside
-             inputProps={ inputProps || { placeholder: "дд.мм.гггг" } } />
-);
+                   className={ className }
+                   timeFormat={ false }
+                   onBlur={
+                      () => {
+                         if (value && validate) {
+                            validate(value);
+                         } else {
+                            Moment.locale("ru");
+                            const formats = ["DD-MM-YYYY", "DD/MM/YYYY", "DD.MM.YYYY"];
+                            if (Moment(value, formats).isValid()) {
+                               onChange && onChange(Moment(value, formats).format("DD.MM.YYYY"));
+                            } else {
+                               onChange && onChange(Moment().format("DD.MM.YYYY"));
+                            }
+                         }
+                      }
+                   }
+                   disableOnClickOutside
+                   inputProps={ inputProps || { placeholder: "дд.мм.гггг" } } />
+      );
+   }
+}
 
 DatePicker.propTypes = {
    className: PropTypes.string,
