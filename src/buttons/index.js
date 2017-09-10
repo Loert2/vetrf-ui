50da15a05c774022b335848732529d011e6654b3
portretov-vs-170320 <portretov-vs-170320@fsvps.ru>
2017-09-10 21:32:15 +0300

    Доделывание кнопки с модалкой и встроенной Textarea, и кнопки с модалкой в котрую можно встроить что угодно

--- src/buttons/index.js
+++ src/buttons/index.js
@@ -2,4 +2,4 @@ export { default as Button } from './button/Button';
 export { default as ButtonLink } from './button-link/ButtonLink';
 export { default as ConfirmActionButton } from './confirm-action/ConfirmActionButton';
 export { default as PromptConfirmActionButton } from './confirm-action/PromptConfirmActionButton';
-export { default as EmbeddedConfirmActionButton } from './confirm-action/EmbeddedConfirmActionButton';
+export { default as CustomActionButton } from './confirm-action/CustomActionButton';
