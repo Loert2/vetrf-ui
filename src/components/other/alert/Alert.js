3abc93eba7ca5a4d78742ddbbe39f30919914d28
morozov-ev-131216 <morozov-ev-131216@fsvps.ru>
2018-01-14 16:48:11 +0300

    Fix кноки закрыть у Alert

--- src/components/other/alert/Alert.js
+++ src/components/other/alert/Alert.js
@@ -1,17 +1,38 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Alert = ({ id, className, closeButton, children }) => (
-   <div id={ id } className={ className || "alert alert-info" }>
-      {
-         closeButton &&
-         <button type="button" className="close" dataDismiss="alert">
-            <i className="ace-icon fa fa-times"/>
-         </button>
+class Alert extends React.PureComponent {
+   constructor() {
+      super();
+      this.state = {
+         showAlert: true
+      };
+      this.closeAlert = this.closeAlert.bind(this);
+   }
+
+   closeAlert() {
+      this.setState({ showAlert: false });
+   }
+
+   render() {
+      const { id, className, closeButton, children } = this.props;
+      if (this.state.showAlert) {
+         return (
+            <div id={ id } className={ className || "alert alert-info" }>
+               {
+                  closeButton &&
+                  <button type="button" className="close" onClick={ this.closeAlert }>
+                     <i className="ace-icon fa fa-times"/>
+                  </button>
+               }
+               { children }
+            </div>
+         );
+      } else {
+         return null;
       }
-      { children }
-   </div>
-);
+   }
+}
 
 Alert.propTypes = {
    children: PropTypes.node,
