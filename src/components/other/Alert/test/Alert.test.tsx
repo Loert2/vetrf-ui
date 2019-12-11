3042b47f57ab9dec44b9ac930c115f2d65ed29b1
shopyrev-dr-180802 <shopyrev-dr-180802@fsvps.ru>
2019-12-11 11:44:11 +0300

    (refs #375743) Написать тестов на корректное формирование className
    Написасие тестов на корректное формирование className для компонентов Alert, Badge, Icon, Label, LoadingSpinner

--- src/components/other/Alert/test/Alert.test.tsx
+++ src/components/other/Alert/test/Alert.test.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import { Alert } from '../Alert';
+import { mount, shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import { create } from 'react-test-renderer';
+import 'jsdom-global/register';
+
+configure({ adapter: new Adapter() });
 
 describe('Alert', () => {
-   it('Alert is displayed correctly', () => {
-      const component = create(<Alert>Сообщение выведено</Alert>);
+   it('should displayed correctly', () => {
+      const component = create(<Alert children="Сообщение выведено" massageType="info" closable />);
+
       const json = component.toJSON();
+
       expect(json).toMatchSnapshot();
    });
-   it('Alert is correctly closing', () => {
-      const component = create(<Alert closable>Сообщение выведено</Alert>);
-      component.root.findByType('button').props.onClick();
-      expect(component).toMatchObject({});
+
+   it('should call onClick correctly', () => {
+      const component = mount(<Alert children="Сообщение выведено" closable />);
+
+      component.find('button').simulate('click');
+
+      expect(component).toEqual({});
+   });
+
+   it('should render children correctly', () => {
+      const component = shallow(<Alert children="Сообщение выведено" />);
+
+      expect(component.text()).toEqual('Сообщение выведено');
+   });
+
+   it('should render button-close correctly', () => {
+      const component = mount(<Alert children="Сообщение выведено" closable />);
+
+      expect(component.find('button').length).toEqual(1);
+   });
+
+   it('with massageType prop should correctly form className', () => {
+      const component = mount(<Alert children="Сообщение выведено" massageType="info" />);
+
+      expect(component.find('div').hasClass('alert-info')).toEqual(true);
    });
 });
